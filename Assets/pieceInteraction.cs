7779e3e1e4558bce89a46b979098100a9d7f1ecb
Quinn Barber <87210541+Quinn-Barber@users.noreply.github.com>
2022-05-22 17:42:45 -0400

    Add files via upload

--- Assets/pieceInteraction.cs
+++ Assets/pieceInteraction.cs
@@ -1,9 +1,12 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class pieceInteraction : MonoBehaviour
 {
+    bool beingHeld = false;
+    Vector2 origPos;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,31 @@ public class pieceInteraction : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    private void OnMouseDrag()
+    {
+        if(beingHeld == false)
+        {
+            origPos = new Vector2(this.gameObject.transform.position.x, this.gameObject.transform.position.y);
+        }
+        beingHeld = true;
+        Vector2 mousePos;
+        mousePos = Input.mousePosition;
+        mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+        this.gameObject.transform.position = mousePos;
+    }
+
+    private void OnMouseUp()
+    {
+        beingHeld = false;
+        Vector2 newPos = new Vector2((float)Math.Round(this.gameObject.transform.position.x), (float)Math.Round(this.gameObject.transform.position.y));
+        if(newPos.x < 1 || newPos.x > 8 || newPos.y < 1 || newPos.y > 8)
+        {
+            this.gameObject.transform.position = origPos;
+            return;
+        }
+        this.gameObject.transform.position = newPos;
     }
 }
